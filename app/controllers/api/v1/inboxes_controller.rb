047c9750410de78048e73ae810975ea2b5820f14
rlemayd <ralemay@miuandes.cl>
2020-05-06 20:44:51 -0400

    first commit

---
+++ app/controllers/api/v1/inboxes_controller.rb
@@ -0,0 +1,74 @@
+class API::V1::InboxesController < APIController
+  before_action :set_inbox, only: [:show, :edit, :update, :destroy]
+
+  # GET /inboxes
+  # GET /inboxes.json
+  def index
+    @inboxes = Inbox.all
+  end
+
+  # GET /inboxes/1
+  # GET /inboxes/1.json
+  def show
+  end
+
+  # GET /inboxes/new
+  def new
+    @inbox = Inbox.new
+  end
+
+  # GET /inboxes/1/edit
+  def edit
+  end
+
+  # POST /inboxes
+  # POST /inboxes.json
+  def create
+    @inbox = Inbox.new(inbox_params)
+
+    respond_to do |format|
+      if @inbox.save
+        format.html { redirect_to @inbox, notice: 'Inbox was successfully created.' }
+        format.json { render :show, status: :created, location: @inbox }
+      else
+        format.html { render :new }
+        format.json { render json: @inbox.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /inboxes/1
+  # PATCH/PUT /inboxes/1.json
+  def update
+    respond_to do |format|
+      if @inbox.update(inbox_params)
+        format.html { redirect_to @inbox, notice: 'Inbox was successfully updated.' }
+        format.json { render :show, status: :ok, location: @inbox }
+      else
+        format.html { render :edit }
+        format.json { render json: @inbox.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /inboxes/1
+  # DELETE /inboxes/1.json
+  def destroy
+    @inbox.destroy
+    respond_to do |format|
+      format.html { redirect_to inboxes_url, notice: 'Inbox was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_inbox
+      @inbox = Inbox.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def inbox_params
+      params.fetch(:inbox, {})
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users do
+        resources :inboxes
+      end
+      resources :events do
+        resources :comments
+      end
+      resources :organizations
+    end
+  end
+
+  resources :users, defaults: { format: :html }
+  resources :comments, defaults: { format: :html }
+  resources :inboxes, defaults: { format: :html }
+  resources :events, defaults: { format: :html }
+  resources :organizations, defaults: { format: :html }
 end
