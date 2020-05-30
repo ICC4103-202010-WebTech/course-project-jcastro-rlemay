bf44f76be775c2690bae98163bb9490f952131b3
Web Tech <jacastro2@miuandes.cl>
2020-05-29 21:54:18 -0400

    implemented the function populate_fake_data

--- test/factories.rb
+++ test/factories.rb
@@ -2,50 +2,53 @@ require 'securerandom'
 
 include FactoryBot::Syntax::Methods
 FactoryBot.define do
+
+  factory :user do
+    name { Faker::FunnyName.name }
+    lastName { Faker::Name.last_name }
+    password { Faker::Code.sin }
+    email {Faker::Internet.safe_email}
+    location {Faker::Nation.capital_city}
+    address {Faker::Address.full_address}
+    phone {Faker::PhoneNumber.cell_phone}
+  end
+
+  factory :event_organizer do
+    user {User.offset(rand(User.count)).first}
+    factory :event_organizer_with_events do
+      transient do
+        events_count { rand(1..5) }
+      end
+
+      after(:create) do |event_organizer, evaluator|
+        create_list(:event, evaluator.events_count, event_organizer: event_organizer)
+      end
+    end
+  end
+
   factory :event do
     name { Faker::Esport.event }
-    if Faker::Boolean
+    if Faker::Boolean.boolean
       start_date { Faker::Date.between(from: Date.today, to: Date.today + 6.months) }
       end_date { Faker::Date.between(from: start_date, to: start_date + 6.months) }
-
     end
     location { Faker::Address.street_address }
     description { Faker::Lorem.sentence }
-    is_public { Faker::Boolean }
-    event_organizer_id { EventOrganizer.offset(rand(EventOrganizer.count)).first.id }
+    is_public { Faker::Boolean.boolean }
+    after(:create) do |event, evaluator|
+      create_list(:poll, Event.where(:start_date => nil).count,event: event, name: event.name+" poll")
+    end
   end
-  factory :event_page do
-    minimumGuests { Faker::Number.within(range: 10..50)}
-    maximumGuests { Faker::Number.within(range: 50..200)}
-    bannerPicture { Faker::Lorem.sentence}
-    event_id { Event.offset(rand(Event.count)).first.id }
-  end
+
   factory :invitation do
     message { Faker::Lorem.sentence}
     user_id { User.offset(rand(User.count)).first.id }
     event_id { Event.offset(rand(Event.count)).first.id }
   end
-  factory :notification do
-    message { Faker::Lorem.sentence}
-    user_id { User.offset(rand(User.count)).first.id }
-  end
+
   factory :poll do
-    x = Event.where(:start_date => nil)
-    name { x.name + " poll" }
-    dates = ""
-
-    t.text { Faker::Date}
+    minimumAnswers { Faker::Number.within(range: 5..20)}
+    possibleDates { Faker::Date.in_date_period(year: 2021).to_s + " " + Faker::Date.in_date_period(year: 2021).to_s+ " " + Faker::Date.in_date_period(year: 2021).to_s}
   end
-  factory :poll_notification do
 
-  end
-  factory :vote do
-
-  end
-  factory :user do
-
-  end
-  factory :user_profile do
-
-  end
 end
