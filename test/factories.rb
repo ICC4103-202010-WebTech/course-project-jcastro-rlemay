6fc8aaff1f249bd1184e9adf6774e5f7fd6b6f2b
webtech <webtech@webtech-debian.webtech>
2020-05-30 13:21:27 -0400

    Factories updated

--- test/factories.rb
+++ test/factories.rb
@@ -18,27 +18,39 @@ FactoryBot.define do
     factory :event_organizer_with_events do
       transient do
         events_count { rand(1..5) }
+        event_with_poll_count { rand(0..2) }
       end
 
       after(:create) do |event_organizer, evaluator|
-        create_list(:event, evaluator.events_count, event_organizer: event_organizer)
+        create_list(:event_without_poll, evaluator.events_count, event_organizer: event_organizer)
+        create_list(:event_with_poll, evaluator.event_with_poll_count, event_organizer: event_organizer)
       end
     end
   end
 
   factory :event do
-    name { Faker::Esport.event }
-    if Faker::Boolean.boolean
+
+    factory :event_without_poll do
+      name { Faker::Esport.event }
       start_date { Faker::Date.between(from: Date.today, to: Date.today + 6.months) }
       end_date { Faker::Date.between(from: start_date, to: start_date + 6.months) }
+      location { Faker::Address.street_address }
+      description { Faker::Lorem.sentence }
+      is_public { Faker::Boolean.boolean }
     end
-    location { Faker::Address.street_address }
-    description { Faker::Lorem.sentence }
-    is_public { Faker::Boolean.boolean }
-    after(:create) do |event, evaluator|
-      create_list(:poll, Event.where(:start_date => nil).count,event: event, name: event.name+" poll")
+    factory :event_with_poll do
+      name { Faker::Esport.event }
+      location { Faker::Address.street_address }
+      description { Faker::Lorem.sentence }
+      is_public { Faker::Boolean.boolean }
+      after(:create) do |event, evaluator|
+        create_list(:poll, 1,event: event, name: event.name+" poll")
+      end
     end
+
   end
+
+
 
   factory :invitation do
     message { Faker::Lorem.sentence}
