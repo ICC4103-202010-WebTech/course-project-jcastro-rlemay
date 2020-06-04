6981f7741dc38e7c0dba20e70eb028dc03a3d3c7
Web Tech <jacastro2@miuandes.cl>
2020-06-04 02:59:01 -0400

    upgraded search and fixed some UI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,14 +7,31 @@ class PagesController < ApplicationController
 
   end
   def search
-    print("holaaaaaaaaaaaaa", params[:query])
-    @users_by_name = User.where("name LIKE ? or lastName LIKE ?", "%"+params[:query].to_s+"%", "%"+params[:query].to_s+"%").limit(5)
-    @users_by_username = User.where(id: UserProfile.where("userName LIKE ?", "%"+params[:query].to_s+"%").pluck(:user_id)).limit(5)
-    @organizations_by_name = Organization.where("name LIKE ?", "%"+params[:query].to_s+"%").limit(5)
-    @events_by_title = Event.where("name LIKE ?", "%"+params[:query].to_s+"%").limit(5)
-    @events_by_desc = Event.where("description LIKE ?", "%"+params[:query].to_s+"%").limit(5)
-    @events_by_creator = Event.where(event_organizer_id: User.where("name LIKE ? or lastName LIKE ?", "%"+params[:query].to_s+"%", "%"+params[:query].to_s+"%").ids).limit(5)
-    @events_by_organization = Event.find(OrganizationEvent.where(organization_id: Organization.where("name LIKE ?", "%"+params[:query].to_s+"%").ids).pluck(:event_id))
+    print("AAAAAAAAAAAA", params[:tags])
+    if params[:tags] == nil
+      @users_by_name = User.where("name LIKE ? or lastName LIKE ?", "%"+params[:query].to_s+"%", "%"+params[:query].to_s+"%").limit(5)
+      @users_by_username = User.where(id: UserProfile.where("userName LIKE ?", "%"+params[:query].to_s+"%").pluck(:user_id)).limit(5)
+      @organizations_by_name = Organization.where("name LIKE ?", "%"+params[:query].to_s+"%").limit(5)
+      @events_by_title = Event.where("name LIKE ?", "%"+params[:query].to_s+"%").limit(5)
+      @events_by_desc = Event.where("description LIKE ?", "%"+params[:query].to_s+"%").limit(5)
+      @events_by_creator = Event.where(event_organizer_id: User.where("name LIKE ? or lastName LIKE ?", "%"+params[:query].to_s+"%", "%"+params[:query].to_s+"%").ids).limit(5)
+      @events_by_organization = Event.find(OrganizationEvent.where(organization_id: Organization.where("name LIKE ?", "%"+params[:query].to_s+"%").ids).pluck(:event_id))
+    elsif params[:tags] == "user by name"
+      @users_by_name = User.where("name LIKE ? or lastName LIKE ?", "%"+params[:query].to_s+"%", "%"+params[:query].to_s+"%")
+    elsif params[:tags] == "user by username"
+      @users_by_username = User.where(id: UserProfile.where("userName LIKE ?", "%"+params[:query].to_s+"%").pluck(:user_id))
+    elsif params[:tags] == "organization by name"
+      @organizations_by_name = Organization.where("name LIKE ?", "%"+params[:query].to_s+"%")
+    elsif params[:tags] == "event by title"
+      @events_by_title = Event.where("name LIKE ?", "%"+params[:query].to_s+"%")
+    elsif params[:tags] == "event by description"
+      @events_by_desc = Event.where("description LIKE ?", "%"+params[:query].to_s+"%").limit(5)
+    elsif params[:tags] == "event by creator"
+      @events_by_creator = Event.where(event_organizer_id: User.where("name LIKE ? or lastName LIKE ?", "%"+params[:query].to_s+"%", "%"+params[:query].to_s+"%").ids).limit(5)
+    elsif params[:tags] == "event by organization"
+      @events_by_organization = Event.find(OrganizationEvent.where(organization_id: Organization.where("name LIKE ?", "%"+params[:query].to_s+"%").ids).pluck(:event_id))
+    end
+
   end
 
 end
