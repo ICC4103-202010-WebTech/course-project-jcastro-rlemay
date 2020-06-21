a12c95b22de9326637a99667b33ca73736da26f8
Richard Le May <ralemay@miuandes.cl>
2020-06-20 21:14:48 -0400

    comments working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -60,13 +60,17 @@ class EventsController < ApplicationController
     end
 
     if EventOrganizer.where(user_id: current_user.id)[0] == nil
-      EventOrganizer.create(user_id: current_user.id)
+      if EventOrganizer.create(user_id: current_user.id)
+        @event = Event.new(name: event_params[:name], start_date: x, end_date: y,
+                           description: event_params[:description],
+                           event_organizer: EventOrganizer.where(user_id: current_user.id)[0], location: event_params[:location])
+      end
+    else
+      @event = Event.new(name: event_params[:name], start_date: x, end_date: y,
+                         description: event_params[:description],
+                         event_organizer: EventOrganizer.where(user_id: current_user.id)[0], location: event_params[:location])
     end
 
-
-    @event = Event.new(name: event_params[:name], start_date: x, end_date: y,
-                       description: event_params[:description],
-                       event_organizer: EventOrganizer.find(current_user.id), location: event_params[:location])
     respond_to do |format|
       if @event.save
         @event_page = @event.event_page
